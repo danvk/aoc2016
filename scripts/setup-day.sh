d7975b1c06ed53a56f8e5da585c190e80414da8b
Dan Vanderkam <danvdk@gmail.com>
2024-12-01 10:43:30 -0500

    cookie in its own file

---
+++ scripts/setup-day.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Sets $header
+. scripts/cookie.sh
+
+day=$1
+dir=input/day$day
+mkdir $dir
+ex_file=lib/day$day.ex
+url="https://adventofcode.com/2016/day/$day"
+cat <<END > $ex_file
+# $url
+defmodule Day$day do
+  def parse_line(line) do
+    String.split(line)
+  end
+
+  def main(input_file) do
+    instrs = Util.read_lines(input_file) |> Enum.map(&parse_line/1)
+    IO.inspect(instrs)
+  end
+end
+END
+
+perl -i -pe '/next day here/ and print "      '$day' -> Day'$day'.main(file)\n"' lib/main.ex
+
+# https://adventofcode.com/2016/day/3/input
+curl "https://adventofcode.com/2016/day/$day/input" \
+  --silent \
+  -H "$header" \
+  > $dir/input.txt
+
+echo 'input:'
+head $dir/input.txt
+
+# https://adventofcode.com/2016/day/4
+curl "$url" \
+  --silent \
+  -H "$header" \
+  > /tmp/advent.html
+
+./scripts/extract-sample.ts /tmp/advent.html $dir
+
+echo ''
+echo "mix escript.build && ./main $day $dir/input.txt"
+echo ''
+echo "https://adventofcode.com/2016/day/$day"
+echo ''
